667d4953e71b4ecb7cf4f0a2cbd38d3e20b6473a
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2022-11-25 15:00:41 +0100

    color

--- resources/js/Components/SearchSlideOver.vue
+++ resources/js/Components/SearchSlideOver.vue
@@ -80,7 +80,7 @@ const hideDialog = () => {
                   <div class="flex h-4/6 flex-col bg-white py-6 shadow-xl">
                     <div class="px-4 sm:px-6">
                       <DialogTitle class="text-lg font-medium text-gray-900 text-right">
-                        <button @click="hideDialog()" type="button" class="text-neutral-500 border border-neutral-500 hover:bg-neutral-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
+                        <button @click="hideDialog()" type="button" class="text-neutral-500 border border-neutral-500 hover:bg-neutral-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-neutral-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800">
                           <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                           <span class="sr-only">Icon description</span>
                         </button>
