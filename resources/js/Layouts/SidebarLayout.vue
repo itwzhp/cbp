1ab7e3dcffbb20d77aef314843f6efcf59f2b4c7
Gacek <szymon.gackowski@gmail.com>
2022-12-17 14:47:29 +0100

    updated vite to v4

---
+++ resources/js/Layouts/SidebarLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import {Link} from '@inertiajs/inertia-vue3'
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <template #header>
+            <Link :href="route('materials.index')" class="text-sm"><i class="fa fa-chevron-left"></i> powrót</Link>
+        </template>
+
+        <div class="flex max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <div class="w-3/4">
+                <slot></slot>
+            </div>
+            <div class="w-1/4">
+                <slot name="sidebar"></slot>
+            </div>
+        </div>
+
+    </AuthenticatedLayout>
+</template>
--- resources/js/Pages/Materials/Show.vue
+++ resources/js/Pages/Materials/Show.vue
@@ -1,38 +1,59 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/inertia-vue3';
 import Attachment from "@/Components/Attachment.vue";
 import TaxonomyBadge from "@/Components/TaxonomyBadge.vue";
+import SidebarLayout from "@/Layouts/SidebarLayout.vue";
 </script>
 
 <template>
     <Head title="Materials"/>
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                <a :href="route('materials.show', $page.props.material.slug)">{{ $page.props.material.title }}</a>
-            </h2>
-            <div v-html="$page.props.material.content"></div>
+    <SidebarLayout>
+        <template #default>
 
-            <TaxonomyBadge
-                v-for="(item, index) in $page.props.material.taxonomies"
-                :key="index"
-                :taxonomy="item"
-            ></TaxonomyBadge>
-
-            <div class="mt-5">
-                <div class="flex justify-between content-center items-center">
-                    <h4 class="text-lg font-semibold mb-2">Załączniki do wydruku</h4>
-                    <a :href="route('materials.download', $page.props.material.slug)">
-                        <i class="fa fa-download"></i> Pobierz wszystkie załączniki
-                    </a>
+            <div class="p-6 flex justify-between align-middle border-b-2 border-gray-200">
+                <div>
+                    <img :src="$page.props.material.owner.avatar" class="inline-block w-6 h-6 rounded-full">
+                    {{ $page.props.material.owner.name }}
                 </div>
-                <Attachment v-for="(attachment, key) in $page.props.material.attachments"
-                            :key="key" :attachment="attachment"></Attachment>
+                <p class="text-base text-gray-500 text-sm">
+                    <i class="fa fa-calendar text-gray-500"></i>
+                    {{ $filters.dateFormat($page.props.material.published_at) }}
+                </p>
+            </div>
 
+            <div class="p-6">
+                <h2 class="font-semibold text-3xl text-gray-800 leading-tight font-medium mb-2">
+                    <a :href="route('materials.show', $page.props.material.slug)">{{ $page.props.material.title }}</a>
+                </h2>
+                <div v-html="$page.props.material.content"></div>
+
+                <TaxonomyBadge
+                    v-for="(item, index) in $page.props.material.taxonomies"
+                    :key="index"
+                    :taxonomy="item"
+                ></TaxonomyBadge>
+
+                <div class="mt-5">
+                    <div class="flex justify-between content-center items-center">
+                        <h4 class="text-lg font-semibold mb-2">Załączniki do wydruku</h4>
+                        <!-- todo: dodać spinner na przycisk pobierania wszystkich załączników - przygotowanie zipa może chwilę potrwać-->
+                        <a :href="route('materials.download', $page.props.material.slug)">
+                            <i class="fa fa-download"></i> Pobierz wszystkie załączniki
+                        </a>
+                    </div>
+                    <Attachment v-for="(attachment, key) in $page.props.material.attachments"
+                                :key="key" :attachment="attachment"
+                    ></Attachment>
+
+                </div>
             </div>
 
         </template>
-    </AuthenticatedLayout>
+
+        <template #sidebar>
+            sidebar
+        </template>
+
+    </SidebarLayout>
 </template>
