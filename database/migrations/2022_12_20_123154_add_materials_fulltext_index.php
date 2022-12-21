d9e1757313d48f3171913cdd62e4278f33fad020
Gacek <szymon.gackowski@gmail.com>
2022-12-21 12:30:16 +0100

    2022-12-21_12-30-15

--- database/migrations/2022_12_20_123154_add_materials_fulltext_index.php
+++ database/migrations/2022_12_20_123154_add_materials_fulltext_index.php
@@ -1,18 +1,26 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up()
     {
-        DB::statement(<<<'SQL'
+        try {
+            DB::statement(<<<'SQL'
             create fulltext index materials_fulltext_index
                 on materials(title, description)
                 with parser ngram
         SQL
-        );
+            );
+        } catch (QueryException $exception) {
+            DB::statement(<<<'SQL'
+ALTER TABLE materials ADD FULLTEXT INDEX `materials_fulltext_index` (title, description);
+SQL
+            );
+        }
     }
 
     public function down()
