c7bc19a504e76e9f2f9aa9d0fa779ef6167f059f
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2022-12-21 14:11:41 +0100

    zhp colors

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,7 +13,7 @@ import { Head } from '@inertiajs/inertia-vue3';
             </h2>
         </template> -->
 
-        <div class="py-12">
+        <div class="py-2">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 border-b border-gray-200">
--- resources/js/Pages/Materials.vue
+++ resources/js/Pages/Materials.vue
@@ -54,7 +54,7 @@ onUnmounted(() => {
 <template>
   <Head title="Materiały" />
   <AuthenticatedLayout>
-    <div class="py-12">
+    <div class="py-2">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 border-b border-gray-200">
