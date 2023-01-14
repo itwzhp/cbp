a2603af3fe80ceaead00e3c687ec55c0b446ff49
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-01-14 14:37:22 +0100

    cosmetics

--- resources/js/Components/Materials/TagSearchMode.vue
+++ resources/js/Components/Materials/TagSearchMode.vue
@@ -14,7 +14,7 @@
 </script>
 
 <template>
-  <ul class="items-center w-1/2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex">
+  <ul class="items-center w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 sm:flex">
       <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
           <div class="flex items-center pl-3">
               <input v-model="mode" id="horizontal-list-radio-license" type="radio" value="or" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
