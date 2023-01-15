ca7f0613a3b07340ec09034c736d2400907fb57d
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-01-15 19:18:32 +0100

    search form to component

---
+++ resources/js/Components/Materials/SearchForm.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { watch } from "@vue/runtime-core";
+  import { useForm } from '@inertiajs/inertia-vue3';
+  import { useSearchStore } from "../../store/search.store";
+
+  const emit = defineEmits(['searchSubmit']);
+  const store = useSearchStore();
+  const form = useForm({search: store.getSearchInput});
+
+  watch(store, (newValue) => {
+    if (!store.getSearchInput) {
+      form.search = null;
+    }
+  });
+
+  const submit = () => emit('searchSubmit', form.search);
+</script>
+
+<template>
+  <form @submit.prevent="submit" class="pb-3">
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input minlength="3" type="search" id="default-search" :disabled="store.getLoading" v-model="form.search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-zhp-300 focus:border-zhp-300" placeholder="Wpisz wyszukiwaną frazę">
+        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-zhp-500 hover:bg-zhp-500 focus:ring-2 focus:outline-none focus:ring-zhp-300 font-medium rounded-lg text-sm px-4 py-2">Szukaj</button>
+    </div>
+  </form>
+</template>
--- resources/js/Components/Materials/SearchSlideOver.vue
+++ resources/js/Components/Materials/SearchSlideOver.vue
@@ -1,35 +1,27 @@
 <script setup>
-  import { watch } from "@vue/runtime-core";
   import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
   import { Inertia } from "@inertiajs/inertia";
-  import { useForm } from '@inertiajs/inertia-vue3';
   import { useSearchStore } from "../../store/search.store";
   import PrimaryButton from '@/Components/PrimaryButton.vue';
   import TextInput from '@/Components/TextInput.vue';
   import InputError from '@/Components/InputError.vue';
   import Taxonomy from '@/Components/Materials/Taxonomy.vue';
   import TagSearchMode from '@/Components/Materials/TagSearchMode.vue';
+  import SearchForm from '@/Components/Materials/SearchForm.vue';
 
   const store = useSearchStore();
   store.getTaxonomies().then();
 
-  const form = useForm({search: store.getSearchInput});
   const redirect = () => {
     if (route().current() !==  'materials.index') {
       Inertia.get(route('materials.index'));
     }
   };
 
-  watch(store, (newValue) => {
-    if (!store.getSearchInput) {
-      form.search = null;
-    }
-  })
-
   const tagSelected = (tag) => store.pushTags([tag]).then(() => redirect());
   const tagRemoved = (tag) => store.removeTags([tag]).then(() => redirect());
   const tagModeChanged = (mode) => store.setTagMode(mode).then(() => redirect());
-  const submit = () => store.getData(form.search, store.getTagIds).then(() => redirect());
+  const searchSubmit = (search) => store.getData(search, store.getTagIds).then(() => redirect());
   const hideDialog = () => store.hideDialog();
 </script>
 <template>
@@ -54,20 +46,12 @@
                     </div>
                     <div class="relative mt-6 flex-1 px-4 sm:px-6">
                       <h3 class="mb-5 font-semibold text-gray-900 text-3xl">Filtry</h3>
-                      <hr class="mt-5 mb-5">
-                      <h3 class="mb-2 font-semibold text-gray-900 text-xl">Fraza</h3>
-                      <form @submit.prevent="submit" class="pb-3">
-                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                            </div>
-                            <input minlength="3" type="search" id="default-search" :disabled="store.getLoading" v-model="form.search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-zhp-300 focus:border-zhp-300" placeholder="Wpisz wyszukiwaną frazę">
-                            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-zhp-500 hover:bg-zhp-500 focus:ring-2 focus:outline-none focus:ring-zhp-300 font-medium rounded-lg text-sm px-4 py-2">Szukaj</button>
-                        </div>
-                      </form>
-                      <hr class="mt-5 mb-5">
                       <div>
+                        <h3 class="mb-2 font-semibold text-gray-900 text-xl">Fraza</h3>
+                        <SearchForm @searchSubmit="searchSubmit($event)" />
+                      </div>
+                      <hr hidden class="mt-5 mb-5">
+                      <div hidden>
                         <h3 class="mb-2 font-semibold text-gray-900 text-xl">Tryb tagów</h3>
                         <TagSearchMode @modeChanged="tagModeChanged($event)" :mode="store.getTagMode" />
                       </div>
@@ -78,15 +62,6 @@
                           <Taxonomy @tagSelected="tagSelected($event)" @tagRemoved="tagRemoved($event)" :item="item" :defaultIds="store.getTagIds" />
                         </template>
                       </div>
-                      <!-- <div id="infinite-scroll" style="overflow:auto; max-height: 400px;" class="mt-5 mb-5 grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                        <MaterialMiniCard
-                          v-for="(item, index) in store.getSearchData" :key="index"
-                          :item="item"
-                        />
-                      </div>
-                      <div v-if="store.getLoading" class="text-center pt-2">
-                        <Spinner />
-                      </div> -->
                     </div>
                   </div>
                 </DialogPanel>
