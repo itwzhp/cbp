67c6d5b8551f1b275e3fbfc35fcbfa4cc9d45056
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-01-20 17:06:58 +0100

    footer mobile

--- resources/js/Layouts/Footer.vue
+++ resources/js/Layouts/Footer.vue
@@ -7,8 +7,8 @@
 <template>
     <footer v-if="showFooter(route().current())" ref="footer" class="flex flex-col bg-zhp-300 pt-4 pb-2.5 text-white">
       <div class="bg-zhp-500">
-        <div class="flex flex-row h-36 items-center max-w-7xl mx-auto">
-          <div class="flex basis-1/2">
+        <div class="md:flex md:flex-row h-40 md:h-36 items-center max-w-7xl mx-auto">
+          <div class="flex basis-1/2 p-4">
             <div>
               <!-- <img style="opacity: 0.2; z-index: -1;" src="/images/questionmarks.svg" alt="questionmarks"> -->
               <p class="text-sm font-bold pb-2">
@@ -19,7 +19,7 @@
               </p>
             </div>
           </div>
-          <div class="flex basis-1/2 justify-end">
+          <div class="flex basis-1/2 justify-center md:justify-end">
             <div class="my-auto">
               <img class="h-auto w-44" src="/images/wagggs-logo.png" alt="wosm-logo" />
             </div>
@@ -31,19 +31,19 @@
       </div>
       <div class="flex justify-center pt-2.5 text-sm">
         <ul class="list-none">
-          <li class="inline-block">
-            <a href="https://zhp.pl/copyright" target="_blank" class="p-1 hover:underline">Copyright© </a>1997-{{year}} Związek Harcerstwa Polskiego |
+          <li class="md:inline-block">
+            <a href="https://zhp.pl/copyright" target="_blank" class="p-1 hover:underline">Copyright© </a>1997-{{year}} Związek Harcerstwa Polskiego <span class="invisible md:visible">|</span>
           </li>
-          <li class="inline-block">
-            <a href="https://zhp.pl/informacje-i-uwagi-prawne" target="_blank" class="p-1 hover:underline">Informacje i uwagi prawne</a>|
+          <li class="md:inline-block">
+            <a href="https://zhp.pl/informacje-i-uwagi-prawne" target="_blank" class="p-1 hover:underline">Informacje i uwagi prawne</a><span class="invisible md:visible">|</span>
           </li>
-          <li class="inline-block">
-            <a href="https://zhp.pl/rodo" target="_blank" class="p-1 hover:underline">Polityka prywatności</a>|
+          <li class="md:inline-block">
+            <a href="https://zhp.pl/rodo" target="_blank" class="p-1 hover:underline">Polityka prywatności</a><span class="invisible md:visible">|</span>
           </li>
-          <li class="inline-block">
-            <a href="https://bip.zhp.pl/" target="_blank" class="p-1 hover:underline">Biuletyn Informacji Publicznej</a>|
+          <li class="md:inline-block">
+            <a href="https://bip.zhp.pl/" target="_blank" class="p-1 hover:underline">Biuletyn Informacji Publicznej</a><span class="invisible md:visible">|</span>
           </li>
-          <li class="inline-block">
+          <li class="md:inline-block">
             <a href="https://zhp.pl/deklaracja-dostepnosci" target="_blank" class="p-1 hover:underline">Deklaracja dostępności</a>
           </li>
         </ul>
