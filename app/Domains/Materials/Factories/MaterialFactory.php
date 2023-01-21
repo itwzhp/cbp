2e422a34ac29023ada667b0bf47079bd71b03b62
Gacek <szymon.gackowski@gmail.com>
2023-01-21 14:05:47 +0100

    added tests for material published scope

--- app/Domains/Materials/Factories/MaterialFactory.php
+++ app/Domains/Materials/Factories/MaterialFactory.php
@@ -4,6 +4,8 @@ namespace App\Domains\Materials\Factories;
 use App\Domains\Materials\Models\Material;
 use App\Domains\Materials\States\Draft;
 use App\Domains\Materials\States\Published;
+use App\Domains\Users\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MaterialFactory extends Factory
@@ -19,6 +21,7 @@ class MaterialFactory extends Factory
             'title'       => fake()->sentence(),
             'description' => fake()->text(),
             'state'       => Draft::class,
+            'user_id'     => User::factory(),
         ];
     }
 
@@ -26,7 +29,8 @@ class MaterialFactory extends Factory
     {
         return $this->state(function (array $attributes) {
             return [
-                'state' => Published::class,
+                'state'        => Published::class,
+                'published_at' => Carbon::now()->subDays(rand(1, 10)),
             ];
         });
     }
