e1bfdee0bcc26ed85629fc7ea9610a8309851882
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-02-01 20:01:13 +0100

    Link fix

--- resources/js/Components/Topic.vue
+++ resources/js/Components/Topic.vue
@@ -1,11 +1,12 @@
 <script setup>
+import { Link } from "@inertiajs/vue3";
 const props = defineProps({
   topic: { type: Object, required: true }
 })
 </script>
 
 <template>
-  <a :href="topic.url"
+  <Link :href="topic.url"
     class="h-20 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
     <div class="ml-3 hidden md:block">
       <div class="relative w-12 h-12 overflow-hidden bg-gray-300 rounded-full"></div>
@@ -13,5 +14,5 @@ const props = defineProps({
     <div class="flex flex-col justify-between p-4 leading-normal mt-2 md:mt-0">
       <h5 class="text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ topic.name }}</h5>
     </div>
-  </a>
+  </Link>
 </template>
