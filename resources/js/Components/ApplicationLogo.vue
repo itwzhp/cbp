f35f671d53001b3c6e3eaf59d479fdcd0d3cc529
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-02-12 15:42:46 +0100

    logo size

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -3,6 +3,6 @@ import { Link } from '@inertiajs/vue3';
 </script>
 <template>
     <Link :href="route('welcome')">
-    <img src="/cbp-logo.png" alt="cbp-logo" class="h-14 min-w-[129px]">
+    <img src="/cbp-logo.png" alt="cbp-logo" class="h-16">
     </Link>
 </template>
