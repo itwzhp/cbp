4dbb3d34009e146de327f51ae52536c042626a12
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-02-21 17:32:18 +0100

    dark mode classes remove

--- resources/js/Components/Admin/PageableTable.vue
+++ resources/js/Components/Admin/PageableTable.vue
@@ -60,7 +60,7 @@
       <div>
         <select
           id="small"
-          class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
         >
           <option value="all">
             Wszystkie
@@ -76,7 +76,7 @@
       <div>
         <select
           id="small"
-          class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
         >
           <option value="all">
             Wszystkie
@@ -96,7 +96,7 @@
         </select>
       </div>
       <div>
-        <div class="bg-white dark:bg-gray-900">
+        <div class="bg-white">
           <label
             for="table-search"
             class="sr-only"
@@ -104,7 +104,7 @@
           <div class="relative">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg
-                class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                class="w-5 h-5 text-gray-500"
                 aria-hidden="true"
                 fill="currentColor"
                 viewBox="0 0 20 20"
@@ -118,15 +118,15 @@
             <input
               id="table-search"
               type="text"
-              class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
               placeholder="Wyszukaj"
             >
           </div>
         </div>
       </div>
     </div>
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <table class="w-full text-sm text-left text-gray-500">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50">
         <tr>
           <th
             scope="col"
@@ -136,7 +136,7 @@
               <input
                 id="checkbox-all-search"
                 type="checkbox"
-                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-2 focus:ring-blue-500"
               >
               <label
                 for="checkbox-all-search"
@@ -187,14 +187,14 @@
           <tr
             v-for="(material, key) in materials"
             :key="key"
-            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            class="bg-white border-b hover:bg-gray-50"
           >
             <td class="w-4 p-4">
               <div class="flex items-center">
                 <input
                   id="checkbox-table-search-1"
                   type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
                 >
                 <label
                   for="checkbox-table-search-1"
@@ -204,7 +204,7 @@
             </td>
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
             >
               {{ material.id }}
             </th>
@@ -223,7 +223,7 @@
             <td class="px-6 py-4">
               <Link
                 :href="route('admin.materials.edit', material.id)"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                class="font-medium text-blue-600 hover:underline"
               >
                 Edytuj
               </Link>
@@ -237,16 +237,16 @@
       class="flex items-center justify-between p-2"
       aria-label="Table navigation"
     >
-      <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Wyniki
-        <span class="font-semibold text-gray-900 dark:text-white">
-          {{ pagination.current_page === 1 ? 1 : pagination.current_page * pagination.count }}-{{ (pagination.current_page === 1 ? 1 : pagination.current_page + 1) * pagination.count }}</span> z <span class="font-semibold text-gray-900 dark:text-white">{{ pagination.total }}
+      <span class="text-sm font-normal text-gray-500">Wyniki
+        <span class="font-semibold text-gray-900">
+          {{ pagination.current_page === 1 ? 1 : pagination.current_page * pagination.count }}-{{ (pagination.current_page === 1 ? 1 : pagination.current_page + 1) * pagination.count }}</span> z <span class="font-semibold text-gray-900">{{ pagination.total }}
         </span>
       </span>
       <ul class="inline-flex items-center -space-x-px">
         <li>
           <a
             v-if="pagination.links?.previous"
-            class="cursor-pointer block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            class="cursor-pointer block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
             @click="prevPage()"
           >
             <span class="sr-only">Poprzednia</span>
@@ -268,51 +268,51 @@
             @click="goToPage(1)"
           >
             <a
-              class="cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
             >1</a>
           </li>
           <li>
             <a
-              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
             >...</a>
           </li>
         </template>
 
         <li>
           <a
-            class="cursor-pointer z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+            class="cursor-pointer z-10 px-3 py-2 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
           >{{ paginationControls.first.pageNumber }}</a>
         </li>
         <li @click="goToPage(paginationControls.middle.pageNumber)">
           <a
             v-if="paginationControls.middle.pageNumber"
-            class="cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            class="cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
           >{{ paginationControls.middle.pageNumber }}</a>
         </li>
         <li @click="goToPage(paginationControls.last.pageNumber)">
           <a
             v-if="paginationControls.last.pageNumber"
             aria-current="page"
-            class="cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            class="cursor-pointer px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
           >{{ paginationControls.last.pageNumber }}</a>
         </li>
         <template v-if="pagination.total_pages !== pagination.current_page && paginationControls.middle.pageNumber !== pagination.total_pages">
           <li>
             <a
-              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
             >...</a>
           </li>
           <li @click="goToPage(pagination.total_pages)">
             <a
               href="#"
-              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              class="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 "
             >{{ pagination.total_pages }}</a>
           </li>
         </template>
         <li>
           <a
             v-if="pagination.links?.next"
-            class="cursor-pointer block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            class="cursor-pointer block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 "
             @click="nextPage()"
           >
             <span class="sr-only">Następna</span>
