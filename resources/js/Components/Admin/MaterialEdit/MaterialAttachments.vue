475b561a1f1f0f70ce647ed40cb872c8e9802c61
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-02-25 18:12:57 +0100

    MaterialAttachments simplify

--- resources/js/Components/Admin/MaterialEdit/MaterialAttachments.vue
+++ resources/js/Components/Admin/MaterialEdit/MaterialAttachments.vue
@@ -1,68 +1,55 @@
 <script setup>
-import {usePage, router} from '@inertiajs/vue3';
+import { router } from '@inertiajs/vue3';
 import vueFilePond from 'vue-filepond';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond/dist/filepond.min.css';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import axios from 'axios';
 
-const material = usePage().props.material;
-const token = usePage().props.token;
-const permissions = usePage().props.permissions;
-
-const FilePond = vueFilePond(
-    FilePondPluginFileValidateType,
-    FilePondPluginImagePreview
-);
+const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
 
 const deleteAttachment = (attachment) => {
-    axios.delete(route('api.admin.materials.attachments.destroy', {
+  axios
+    .delete(
+      route('api.admin.materials.attachments.destroy', {
         material: attachment.material_id,
         attachment: attachment.id
-    }))
-        .then(() => {
-            // TODO: to można zrobić jakoś lepiej?
-            material.attachments = material.attachments.filter((item) => {
-                return item.id !== attachment.id;
-            });
-        });
-}
+      })
+    )
+};
 
 const refreshFiles = () => {
-    // TODO: to działało dopóki nie było w komponencie. Jak wyniosłem te attachments do komponentu, to przestało działać
-    router.reload({only: ['material']});
-}
+  router.reload({ only: ['material'] });
+};
 </script>
 <template>
   <ul>
     <li
-      v-for="attachment in material.attachments"
+      v-for="attachment in $page.props.material.attachments"
       :key="attachment.id"
     >
       {{ attachment }}
       <button
-        v-if="permissions.includes('update')"
+        v-if="$page.props.permissions.includes('update')"
         class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-xs"
         @click.prevent="deleteAttachment(attachment)"
       >
-        <FontAwesomeIcon
-          icon="trash"
-        />
+        <FontAwesomeIcon icon="trash" />
       </button>
     </li>
   </ul>
   <FilePond
-    v-if="permissions.includes('update')"
+    v-if="$page.props.permissions.includes('update')"
     ref="pond"
     name="attachments"
     label-idle="Dodaj pliki do swojego materiału"
     :server="{
-      url: route('api.admin.materials.attachments.store', material.id),
+      url: route('api.admin.materials.attachments.store', $page.props.material.id),
       withCredentials: true,
       headers: {
-        'X-CSRF-TOKEN': token,
+        'X-CSRF-TOKEN': $page.props.token
       }
     }"
     :allow-multiple="true"
