31d2d074603a3eca5dd4565c587e21c5658ffa99
Piotr Jagielski <jagielski.piotr.93@gmail.com>
2023-02-25 20:37:59 +0100

    download attachment

--- resources/js/Components/Admin/MaterialEdit/MaterialAttachments.vue
+++ resources/js/Components/Admin/MaterialEdit/MaterialAttachments.vue
@@ -7,6 +7,7 @@ import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond/dist/filepond.min.css';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import fileDownload from 'js-file-download';
 import axios from 'axios';
 import ContentAccess from '@/Components/Admin/ContentAccess.vue';
 import { permissions } from '@/Components/Admin/permissions.js';
@@ -15,6 +16,7 @@ const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImage
 const refreshFiles = () => {
   router.reload({ only: ['material'] });
 };
+
 const deleteInProgressFileIndex = ref(null);
 const deleteAttachment = (attachment, index) => {
   deleteInProgressFileIndex.value = index;
@@ -30,6 +32,26 @@ const deleteAttachment = (attachment, index) => {
       refreshFiles();
     })
     .catch(() => deleteInProgressFileIndex.value = null)
+};
+
+const downloadInProgressFileIndex = ref(null);
+const downloadAttachment = (attachment, index) => {
+  downloadInProgressFileIndex.value = index;
+  axios
+    .get(
+      route('api.admin.materials.attachments.show', {
+        material: attachment.material_id,
+        attachment: attachment.id
+      })
+    )
+    .then((response) => {
+      downloadInProgressFileIndex.value = null;
+      fileDownload(
+        response.data,
+        response.headers['content-disposition'].split('filename=')[1]
+    );
+    })
+    .catch(() => downloadInProgressFileIndex.value = null)
 };
 </script>
 <template>
@@ -53,12 +75,15 @@ const deleteAttachment = (attachment, index) => {
             <div>{{ attachment.name }}</div>
           </div>
           <div class="flex items-center">
-            <a
+            <button
+              :disabled="downloadInProgressFileIndex === attachment.id"
+              :class="{'bg-blue-500/50 hover:bg-blue-500/50 cursor-wait': downloadInProgressFileIndex === attachment.id}"
               class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs mx-2"
               :href="attachment.download_url"
+              @click.prevent="downloadAttachment(attachment, attachment.id)"
             >
               <FontAwesomeIcon icon="file-arrow-down" />
-            </a>
+            </button>
             <ContentAccess :permissions="[permissions.UPDATE]">
               <button
                 :disabled="deleteInProgressFileIndex === attachment.id"
