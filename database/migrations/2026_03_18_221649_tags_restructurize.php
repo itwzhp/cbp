ca1675427149eeb94c884deb386dbf1e5521cd9d
gacek <szymon.gackowski@gmail.com>
2026-03-18 23:26:37 +0100

    update tags migration

---
+++ database/migrations/2026_03_18_221649_tags_restructurize.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Domains\Materials\Models\Tag;
+use App\Domains\Materials\Models\Taxonomy;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $tax = Taxonomy::where('slug', 'zorganizowanie-programu-harcerskiego')->first();
+        if ($tax!== null) {
+            $tax->update(['slug' => 'plan-pracy']);
+        }
+
+        $tax = Taxonomy::where('slug', 'plan-pracy')->first();
+        $slugs = [
+            'planowanie-w-druzynie',
+            'oboz',
+        ];
+
+        $tags = Tag::findBySlugs($slugs)->update([
+            'taxonomy_id' => $tax->id,
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $tax = Taxonomy::where('slug', 'plan-pracy')->first();
+        $tax->update(['slug' => 'zorganizowanie-programu-harcerskiego']);
+
+        $tax = Taxonomy::where('slug', 'harcerski-system-wychowawczy')->first();
+        $slugs = [
+            'planowanie-w-druzynie',
+            'oboz',
+        ];
+
+        $tags = Tag::findBySlugs($slugs)->update([
+            'taxonomy_id' => $tax->id,
+        ]);
+    }
+};
